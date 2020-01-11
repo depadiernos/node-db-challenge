eb1a6b30541c0efcd85cf595603d058b1728bc83
Dennis Padiernos <depadiernos@gmail.com>
2020-01-10 19:06:26 -0600

    create db access methods for find and add

---
+++ tasks/tasks-model.js
@@ -0,0 +1,27 @@
+const db = require("../data/db-config")
+
+const find = async () => {
+  const tasks = await db("tasks")
+  return tasks.map((task) => {
+    return { ...task, completed: task.completed === 1 ? true : false }
+  })
+}
+
+const findById = () => {}
+
+const add = async (body) => {
+  const [id] = await db("tasks").insert(body)
+  return await findById(id)
+}
+
+const update = () => {}
+
+const remove = () => {}
+
+module.exports = {
+  find,
+  findById,
+  add,
+  update,
+  remove
+}
