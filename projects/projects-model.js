0a242b4cc422e6d7fef26c70d3dbc837334a26e3
Dennis Padiernos <depadiernos@gmail.com>
2020-01-10 19:44:26 -0600

    implement stretch endpoint

--- projects/projects-model.js
+++ projects/projects-model.js
@@ -1,4 +1,6 @@
 const db = require("../data/db-config")
+const Task = require("../tasks/tasks-model")
+const Resources = require("../resources/resources-model")
 
 const find = async () => {
   const projects = await db("projects")
@@ -8,8 +10,20 @@ const find = async () => {
 }
 
 const findById = async (id) => {
-  const project = await db("projects").where({ id }).first()
-  return { ...project, completed: project.completed === 1 ? true : false }
+  const project = await db("projects")
+    .where({ id })
+    .first()
+  const _tasks = await db("tasks")
+    .where({ id })
+    .select("description", "notes", "completed")
+  const tasks = _tasks.map((task) => {
+    return { ...task, completed: task.completed === 1 ? true : false }
+  })
+  const resources = await db("resources as r")
+    .join("projects_resources as pr", "pr.resource_id", "r.id")
+    .where("pr.project_id", id)
+    .select("r.id", "r.name", "r.description")
+  return { ...project, tasks, resources, completed: project.completed === 1 ? true : false }
 }
 
 const add = async (body) => {
