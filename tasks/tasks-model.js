ab109e6fb9af0b7583fe3e43d4b5a6456f49deb6
Dennis Padiernos <depadiernos@gmail.com>
2020-01-10 19:44:11 -0600

    add the additional columns for task model

--- tasks/tasks-model.js
+++ tasks/tasks-model.js
@@ -1,14 +1,14 @@
 const db = require("../data/db-config")
 
 const find = async () => {
-  const tasks = await db("tasks")
+  const tasks = await db("tasks as t").join("projects as p", "t.project_id", "p.id").select("t.description", "t.notes", "t.completed", "p.name as project_name", "p.description as project_description")
   return tasks.map((task) => {
     return { ...task, completed: task.completed === 1 ? true : false }
   })
 }
 
 const findById = async (id) => {  
-  const [task] = await db("tasks").where({ id })
+  const [task] = await db("tasks as t").where("t.id", id ).join("projects as p", "t.project_id", "p.id").select("t.description", "t.notes", "t.completed", "p.name as project_name", "p.description as project_description")
   return { ...task, completed: task.completed === 1 ? true : false }
 }
 
